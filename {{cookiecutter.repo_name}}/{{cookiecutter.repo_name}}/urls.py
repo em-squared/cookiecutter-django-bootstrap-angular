4631a83cac74aa040e30f40808ac4c93b6a7cff3
Maxime Moraine <maxime.moraine@gmail.com>
2014-04-15 12:00:19 +0200

    example angular view

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
+from django.views.generic import TemplateView
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -12,5 +14,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # Home
+    (r'^$', TemplateView.as_view(template_name='home.html')),
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
